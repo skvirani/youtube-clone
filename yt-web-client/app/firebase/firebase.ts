dacdf19f080d72311905c12f500e8d12c0d72511
Samad Karim Virani <virani.samad@gmail.com>
2023-08-06 17:39:32 -0500

    added firebase config

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { 
+    getAuth, 
+    signInWithPopup, 
+    GoogleAuthProvider, 
+    onAuthStateChanged, 
+    User 
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCcLy1s-v3ozCA2Ah923SufbK2S--MSS8s",
+  authDomain: "skvirani-yt-clone.firebaseapp.com",
+  projectId: "skvirani-yt-clone",
+  appId: "1:152342665131:web:1d67fb4c3da78056f3e4ee",
+  measurementId: "G-1JX058XVB4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+export function signOut() {
+    return auth.signOut();
+}
+
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback);
+}
