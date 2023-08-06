e36638e11aacac34f5f0cd4dc1d2b14d9bcbeabf
Samad Karim Virani <virani.samad@gmail.com>
2023-08-06 17:28:49 -0500

    Added navbar with logo to take user to root.

---
+++ yt-web-client/app/navbar/navbar.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './navbar.module.css'
+
+export default function Navbar() {
+    return (
+        <nav className={styles.nav}>
+            <Link href={"/"} className={styles.logoContainer}>
+                <Image width={90} height={20}
+                    src={"/youtube-logo.svg"} alt='YouTube logo' 
+                />
+            </Link>
+        </nav>
+    );
+}
