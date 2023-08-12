f73bbee0e09fe4d4d90133e9e12a11a6cce1c6be
Samad Karim Virani <virani.samad@gmail.com>
2023-08-12 16:23:41 -0500

    Added sign in with google

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,16 +1,38 @@
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
 
-import styles from './navbar.module.css'
+import SignIn from "./sign-in";
+import Link from "next/link";
 
-export default function Navbar() {
-    return (
-        <nav className={styles.nav}>
-            <Link href={"/"} className={styles.logoContainer}>
-                <Image width={90} height={20}
-                    src={"/youtube-logo.svg"} alt='YouTube logo' 
-                />
-            </Link>
-        </nav>
-    );
+import styles from "./navbar.module.css";
+import { useEffect, useState } from "react";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
+
+
+function NavBar() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [] /* No dependencies, never rerun */);
+
+
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <span className={styles.logoContainer}>
+          <img className={styles.logo} src="/youtube-logo.svg" alt="YouTube Logo" />
+        </span>
+      </Link>
+      {/* TODO: add upload button */}
+      <SignIn user={user} />
+    </nav>
+  );
 }
+
+export default NavBar;
